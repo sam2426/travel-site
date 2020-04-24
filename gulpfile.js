e0fadf4aacd98665592105e7d1348a0b10fb6e9e
sam2426 <shubhamshaw6@gmail.com>
2020-04-24 21:37:41 +0530

    gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require("gulp");
+var watch = require('gulp-watch');
 
 gulp.task("default", () => {
   console.log("hooray crreated a gulp task");
 });
+
+gulp.task("html", ()=>{
+  console.log("Imagine something useful being done to our html done here!!")
+});
+
+gulp.task("styles", ()=>{
+  console.log("Imagine Sass or PostCss tasks running here!!")
+});
+
+gulp.task('watch', ()=>{
+  watch('./app/index.html',()=>{
+    gulp.start('html');
+  });
+  watch('./app/assets/css/**/*.css',()=>{
+    gulp.start('styles');
+  });
+
+})
